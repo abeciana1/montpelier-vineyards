dcf03f3d08a7557416e41f14be11c4f24896bad0
Alex <alex.beciana@gmail.com>
2021-10-04 23:42:43 -0400

    Created and styled desktop version of nav bar

--- components/utils/NavBar.js
+++ components/utils/NavBar.js
@@ -1,12 +1,63 @@
 import React from 'react';
+import Link from 'next/link'
+
+// import Image from 'next/image';
+
+// import Logo from '../../public/Montpelier_Vinyards_logo.svg'
 
 const NavBar = (props) => {
 
     return (
         <React.Fragment>
-
+            <div
+                className="py-5 px-10 flex items-stretch"
+            >
+                <div className="w-1/3">
+                    <img
+                        src="./Montpelier_Vinyards_logo.svg"
+                        alt="Montpelier Vineyards logo"
+                    />
+                </div>
+                <span
+                    className="text-xl self-center absolute right-10"
+                >
+                        <Link
+                            href="/"
+                        >
+                            <a>Home</a>
+                        </Link>
+                        <Link
+                            href="/our-wines"
+                        >
+                            <a
+                                className="pl-5"
+                            >Our Wines</a>
+                        </Link>
+                        <Link
+                            href="/where-to-buy"
+                        >
+                            <a
+                                className="pl-5"
+                            >Where to Buy</a>
+                        </Link>
+                        <Link
+                            href="/about"
+                        >
+                            <a
+                                className="pl-5"
+                            >About</a>
+                        </Link>
+                        <Link
+                            href="/shop-now"
+                        >
+                            <a
+                                className="pl-5"
+                            >Shop Now</a>
+                        </Link>
+                </span>
+            </div>
         </React.Fragment>
     )
 }
 
-NavBar
+export default NavBar
