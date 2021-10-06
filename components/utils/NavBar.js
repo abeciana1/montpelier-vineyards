2923ea516da8b0e25e34f342b1fd60d56878cb11
Alex <alex.beciana@gmail.com>
2021-10-05 21:15:22 -0400

    WIP — responsive mobile / tablet rendering of nav menu

--- components/utils/NavBar.js
+++ components/utils/NavBar.js
@@ -10,16 +10,16 @@ const NavBar = (props) => {
     return (
         <React.Fragment>
             <div
-                className="py-5 px-16 flex items-stretch"
+                className="py-5 px-5 lg:px-16 flex items-stretch"
             >
-                <div className="w-1/3">
+                <div className="w-8/12 md:w-1/4 md:w-1/3">
                     <img
                         src="./Montpelier_Vinyards_logo.svg"
                         alt="Montpelier Vineyards logo"
                     />
                 </div>
                 <span
-                    className="text-xl text-marv self-center absolute right-16"
+                    className="md:text-lg lg:text-xl text-marv self-center absolute md:right-5 lg:right-16 hidden md:block"
                 >
                         <Link
                             href="/"
@@ -30,30 +30,35 @@ const NavBar = (props) => {
                             href="/our-wines"
                         >
                             <a
-                                className="pl-10"
+                                className="pl-5 lg:pl-10"
                             >Our Wines</a>
                         </Link>
                         <Link
                             href="/where-to-buy"
                         >
                             <a
-                                className="pl-10"
+                                className="pl-5 lg:pl-10"
                             >Where to Buy</a>
                         </Link>
                         <Link
                             href="/about"
                         >
                             <a
-                                className="pl-10"
+                                className="pl-5 lg:pl-10"
                             >About</a>
                         </Link>
                         <Link
                             href="/shop-now"
                         >
                             <a
-                                className="pl-10"
+                                className="pl-5 lg:pl-10"
                             >Shop Now</a>
                         </Link>
+                </span>
+                <span
+                    className="block md:hidden absolute top-5 right-5"
+                >
+
                 </span>
             </div>
         </React.Fragment>
