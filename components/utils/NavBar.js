fa40f656411a0f323151a553aaed65e3b1efb6a9
Alex <alex.beciana@gmail.com>
2021-10-05 22:42:04 -0400

    Rendered responsive mobile nav

--- components/utils/NavBar.js
+++ components/utils/NavBar.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Link from 'next/link'
-
-import { MenuIcon } from '@heroicons/react/solid'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 // import Image from 'next/image';
 
@@ -58,11 +58,85 @@ const NavBar = (props) => {
                         </Link>
                 </span>
                 <span
-                    className="block md:hidden absolute right-5 self-center"
+                    className="block md:hidden absolute right-5 self-center focus:outline-none"
                 >
-                    <MenuIcon
-                        className="text-marv w-8 h-8"
-                    />
+                    <Popover
+                        className="relative"
+                    >
+                        {({ open }) => (
+                            <>
+                                <Popover.Button
+                                    as="button"
+                                >
+                                    {open ? 
+                                <XIcon 
+                                    className="text-marv w-8 h-8 focus:outline-none"
+                                />
+                                :
+                                <MenuIcon
+                                    className="text-marv w-8 h-8 focus:outline-none"
+                                />
+                                }
+                            </Popover.Button>
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-200"
+                                enterFrom="opacity-0 translate-y-1"
+                                enterTo="opacity-100 translate-y-0"
+                                leave="transition ease-in duration-150"
+                                leaveFrom="opacity-100 translate-y-0"
+                                leaveTo="opacity-0 translate-y-1"
+                            >
+                                <Popover.Panel
+                                    as="div"
+                                    className="absolute mx-auto w-56 z-30 mt-3 right-8 sm:px-0"
+                                >
+                                    <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                                        <div className="relative bg-white p-7">
+                                            <ul>
+                                                <li>
+                                                    <Link
+                                                        href="/"
+                                                    >
+                                                        <a>Home</a>
+                                                    </Link>
+                                                </li>
+                                                <li className="pt-3">
+                                                    <Link
+                                                        href="/our-wines"
+                                                    >
+                                                        <a>Our Wines</a>
+                                                    </Link>
+                                                </li>
+                                                <li className="pt-3">
+                                                    <Link
+                                                        href="/where-to-buy"
+                                                    >
+                                                        <a>Where to Buy</a>
+                                                    </Link>
+                                                </li>
+                                                <li className="pt-3">
+                                                    <Link
+                                                        href="/about"
+                                                    >
+                                                        <a>About</a>
+                                                    </Link>
+                                                </li>
+                                                <li className="pt-3">
+                                                    <Link
+                                                        href="/shop-now"
+                                                    >
+                                                        <a>Shop Now</a>
+                                                    </Link>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </Popover.Panel>
+                            </Transition>
+                            </>
+                        )}
+                    </Popover>
                 </span>
             </div>
         </React.Fragment>
