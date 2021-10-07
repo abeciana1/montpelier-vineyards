27fbc4dae4df2bdab30ad47c3707680b1f71b2ff
Alex <alex.beciana@gmail.com>
2021-10-06 22:14:44 -0400

    Added MV favicon to head and title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-      
+      <Head>
+        <title>Montpelier Vineyards</title>
+        <meta property="og:title" content="Montpelier Vineyards" key="title" />
+        <link rel="icon" href="/mv_wine_favicon.svg" />
+      </Head>
     </>
       )
 }
