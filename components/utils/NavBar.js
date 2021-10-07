515bbbc3729a97d1f21faff124d3bc0b347c8a34
Alex <alex.beciana@gmail.com>
2021-10-06 21:57:46 -0400

    Completed repsonsive mobile to desktop nav menus

--- components/utils/NavBar.js
+++ components/utils/NavBar.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/solid'
@@ -9,136 +9,137 @@ import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const NavBar = (props) => {
 
+    const [ showMenu, setShowMenu ]  = useState(false)
+
     return (
         <React.Fragment>
             <div
                 className="py-5 px-5 lg:px-16 flex items-stretch content-center"
             >
-                <div className="w-8/12 md:w-1/4 md:w-1/3">
+                <div
+                    className="flex items-stretch content-center xl:justify-between"
+                >
                     <img
                         src="./Montpelier_Vinyards_logo.svg"
                         alt="Montpelier Vineyards logo"
+                        className="w-8/12 md:w-1/4 md:w-1/3"
                     />
-                </div>
-                <span
-                    className="md:text-lg lg:text-xl text-marv self-center absolute md:right-5 lg:right-16 hidden md:block"
-                >
-                        <Link
-                            href="/"
-                        >
-                            <a>Home</a>
-                        </Link>
-                        <Link
-                            href="/our-wines"
-                        >
-                            <a
-                                className="pl-5 lg:pl-10"
-                            >Our Wines</a>
-                        </Link>
-                        <Link
-                            href="/where-to-buy"
-                        >
-                            <a
-                                className="pl-5 lg:pl-10"
-                            >Where to Buy</a>
-                        </Link>
-                        <Link
-                            href="/about"
-                        >
-                            <a
-                                className="pl-5 lg:pl-10"
-                            >About</a>
-                        </Link>
-                        <Link
-                            href="/shop-now"
-                        >
-                            <a
-                                className="pl-5 lg:pl-10"
-                            >Shop Now</a>
-                        </Link>
-                </span>
-                <span
-                    className="block md:hidden absolute right-5 self-center focus:outline-none"
-                >
-                    <Popover
-                        className="relative"
+                    <span
+                        className="md:text-lg lg:text-xl xl:text-3xl text-marv self-center absolute xl:relative md:right-5 lg:right-16 xl:right-0 hidden md:block"
                     >
-                        {({ open }) => (
-                            <>
-                                <Popover.Button
-                                    as="button"
-                                >
-                                    {open ? 
-                                <XIcon 
-                                    className="text-marv w-8 h-8 focus:outline-none"
-                                />
-                                :
-                                <MenuIcon
-                                    className="text-marv w-8 h-8 focus:outline-none"
-                                />
-                                }
-                            </Popover.Button>
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-200"
-                                enterFrom="opacity-0 translate-y-1"
-                                enterTo="opacity-100 translate-y-0"
-                                leave="transition ease-in duration-150"
-                                leaveFrom="opacity-100 translate-y-0"
-                                leaveTo="opacity-0 translate-y-1"
+                            <Link
+                                href="/"
                             >
-                                <Popover.Panel
+                                <a>Home</a>
+                            </Link>
+                            <Link
+                                href="/our-wines"
+                            >
+                                <a
+                                    className="pl-5 lg:pl-10"
+                                >Our Wines</a>
+                            </Link>
+                            <Link
+                                href="/where-to-buy"
+                            >
+                                <a
+                                    className="pl-5 lg:pl-10"
+                                >Where to Buy</a>
+                            </Link>
+                            <Link
+                                href="/about"
+                            >
+                                <a
+                                    className="pl-5 lg:pl-10"
+                                >About</a>
+                            </Link>
+                            <Link
+                                href="/shop-now"
+                            >
+                                <a
+                                    className="pl-5 lg:pl-10"
+                                >Shop Now</a>
+                            </Link>
+                    </span>
+                    <span
+                        className="self-center absolute right-5"
+                    >
+                        <button
+                            className="text-marv w-8 h-8 md:hidden"
+                            onClick={() => setShowMenu(!showMenu)}
+                        >
+                            {showMenu ?
+                                <XIcon/>
+                                :
+                                <MenuIcon/>
+                            }
+                        </button>
+                    </span>
+                </div>
+            </div>
+            <span
+                    className="navbar block md:hidden self-center relative mx-auto"
+                    >
+                            {showMenu ?
+                            <Transition
+                                    show={showMenu}
                                     as="div"
-                                    className="absolute mx-auto w-56 z-30 mt-3 right-8 sm:px-0"
+                                    enter="transition ease-out duration-200"
+                                    enterFrom="opacity-0 translate-y-1"
+                                    enterTo="opacity-100 translate-y-0"
+                                    leave="transition ease-in duration-150"
+                                    leaveFrom="opacity-100 translate-y-0"
+                                    leaveTo="opacity-0 translate-y-1"
                                 >
-                                    <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                                        <div className="relative bg-white p-7">
-                                            <ul>
-                                                <li>
-                                                    <Link
-                                                        href="/"
-                                                    >
-                                                        <a>Home</a>
-                                                    </Link>
-                                                </li>
-                                                <li className="pt-3">
-                                                    <Link
-                                                        href="/our-wines"
-                                                    >
-                                                        <a>Our Wines</a>
-                                                    </Link>
-                                                </li>
-                                                <li className="pt-3">
-                                                    <Link
-                                                        href="/where-to-buy"
-                                                    >
-                                                        <a>Where to Buy</a>
-                                                    </Link>
-                                                </li>
-                                                <li className="pt-3">
-                                                    <Link
-                                                        href="/about"
-                                                    >
-                                                        <a>About</a>
-                                                    </Link>
-                                                </li>
-                                                <li className="pt-3">
-                                                    <Link
-                                                        href="/shop-now"
-                                                    >
-                                                        <a>Shop Now</a>
-                                                    </Link>
-                                                </li>
-                                            </ul>
+                                    <div
+                                        // className="absolute z-30 mt-5 sm:mt-10 w-72 -right-1 xs:right-6 xsm:right-12 sm:right-1/3"
+                                        className="relative mx-auto w-[320px]"
+                                    >
+                                        <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 mx-10">
+                                            <div className="relative bg-white p-7">
+                                                <ul>
+                                                    <li>
+                                                        <Link
+                                                            href="/"
+                                                        >
+                                                            <a>Home</a>
+                                                        </Link>
+                                                    </li>
+                                                    <li className="pt-3">
+                                                        <Link
+                                                            href="/our-wines"
+                                                        >
+                                                            <a>Our Wines</a>
+                                                        </Link>
+                                                    </li>
+                                                    <li className="pt-3">
+                                                        <Link
+                                                            href="/where-to-buy"
+                                                        >
+                                                            <a>Where to Buy</a>
+                                                        </Link>
+                                                    </li>
+                                                    <li className="pt-3">
+                                                        <Link
+                                                            href="/about"
+                                                        >
+                                                            <a>About</a>
+                                                        </Link>
+                                                    </li>
+                                                    <li className="pt-3">
+                                                        <Link
+                                                            href="/shop-now"
+                                                        >
+                                                            <a>Shop Now</a>
+                                                        </Link>
+                                                    </li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
-                                </Popover.Panel>
-                            </Transition>
-                            </>
-                        )}
-                    </Popover>
-                </span>
-            </div>
+                                </Transition>
+                            : null}
+                    </span>
         </React.Fragment>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,21 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
     colors: {
       black: '#0f161a'
     },
     extend: {
+      screens: {
+        xs: '375px',
+        xsm: '425px',
+      },
       colors: {
         beige: {
           DEFAULT: '#ffefbd'
